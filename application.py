3555702a32163b827cb8f15c732d43961feb0892
Danesh Lachman <daneshlachman@gmail.com>
2021-02-01 04:03:54 +0100

    minor change

--- application.py
+++ application.py
@@ -1,66 +1,75 @@
 import tkinter as Tkinter
+from tkinter import messagebox
 from PIL import Image, ImageDraw, ImageTk
-import pdb
 import numpy as np
-import matplotlib.pyplot as plt
+
+from model import Model
 
 
-class Application():
+class Application:
     def __init__(self):
         # create root window
         self.root = Tkinter.Tk()
+        self.root.title('Handwritten digit recognizer')
+
+        # variable definitions
         self.first_image = True
         self.image = None
         self.image_draw = None
+        self.application_width_and_height = 400
+        self.drawing_line_thickness = 17
+        self.model = Model()
+
+        # call define and train model function to get the model ready
+        self.model.define_and_train_model()
 
         # create canvas to display future image on
-        self.canvas = Tkinter.Canvas(width=512, height=512)
+        self.canvas = Tkinter.Canvas(width=self.application_width_and_height,
+                                     height=self.application_width_and_height)
         self.canvas.pack()
-
-        # call function to create black image on the canvas
-        self.create_new_black_image()
+        self.create_black_image()
 
         # use tag_bind to trigger pain_img to draw ellipse using the mouse
-        self.canvas.tag_bind(self.canvas._image_id, "<B1-Motion>", lambda e: self.paint_img(e))
+        self.canvas.tag_bind(self.canvas._image_id, "<B1-Motion>", lambda e: self.paint_image(e))
 
         # definition of buttons and their triggered functions
-        clear_button = Tkinter.Button(self.root, text="Clear image", command=self.create_new_black_image)
-        predict_button = Tkinter.Button(self.root, text="Predict", command=self.predict)
+        clear_button = Tkinter.Button(self.root, text="Clear image", command=self.create_black_image)
         clear_button.pack()
+        predict_button = Tkinter.Button(self.root, text="Predict", command=self.predict)
         predict_button.pack()
 
         self.root.mainloop()
 
-    def create_new_black_image(self):
+    def create_black_image(self):
+        # if a black image was already created before, use the current image and make all the pixels black
         if not self.first_image:
             pixels = self.image.load()
             for i in range(self.image.size[0]):
                 for j in range(self.image.size[1]):
                     pixels[i, j] = 0
+            self.canvas._image_tk = ImageTk.PhotoImage(self.image)
+            self.canvas.itemconfigure(self.canvas._image_id, image=self.canvas._image_tk)
         else:
             # create black image within the earlier defined canvas
-            self.image = Image.new('L', (512, 512), color="Black")
+            self.image = Image.new('L', (self.application_width_and_height,
+                                         self.application_width_and_height), color="Black")
             self.image_draw = ImageDraw.Draw(self.image)
             self.canvas._image_tk = ImageTk.PhotoImage(self.image)
             self.canvas._image_id = self.canvas.create_image(0, 0, image=self.canvas._image_tk, anchor='nw')
             self.first_image = False
 
-    def paint_img(self, event):
+    def paint_image(self, event):
+        # draw ellipses with the movement of the mouse while its left button is clicked
         x, y = event.x, event.y
-        self.image_draw.ellipse((x-10, y-10, x+10, y+10), fill='white')
+        self.image_draw.ellipse((x-self.drawing_line_thickness, y-self.drawing_line_thickness,
+                                 x+self.drawing_line_thickness, y+self.drawing_line_thickness), fill='white')
         self.canvas._image_tk = ImageTk.PhotoImage(self.image)
         self.canvas.itemconfigure(self.canvas._image_id, image=self.canvas._image_tk)
 
     def predict(self):
+        # resize the drawn image and then make a prediction
         resized_image = self.image.resize((28, 28))
-        # resized_image.show()
-        numpy_array = np.array(resized_image)
-        plt.figure()
-        plt.imshow(numpy_array)
-        plt.colorbar()
-        plt.grid(False)
-        plt.show()
-
-
-Application()
-
+        numpy_conversed_image = np.array(resized_image)
+        prediction, prediction_percentage = self.model.make_prediction(numpy_conversed_image)
+        messagebox.showinfo("Digit Prediction", ("The predicted digit is " + str(prediction) + " with " + str(prediction_percentage) +
+                                           "% accuracy" ))
