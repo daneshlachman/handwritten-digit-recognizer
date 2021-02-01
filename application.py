8aad6bc54772216e10a1e2ca7eb0a89b33979b19
Danesh Lachman <daneshlachman@gmail.com>
2021-02-01 04:02:06 +0100

    working project

--- application.py
+++ application.py
@@ -1,16 +1,66 @@
-import PySimpleGUI as sg
+import tkinter as Tkinter
+from PIL import Image, ImageDraw, ImageTk
+import pdb
+import numpy as np
+import matplotlib.pyplot as plt
 
-sg.theme('DarkAmber')   # Add a little color to your windows
-# All the stuff inside your window. This is the PSG magic code compactor...
-layout = [  [sg.Text('Draw something')],
-            [sg.OK(), sg.Cancel()]]
 
-# Create the Window
-window = sg.Window('Handwritten digit predicter', layout)
-# Event Loop to process "events"
-while True:
-    event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Cancel'):
-        break
+class Application():
+    def __init__(self):
+        # create root window
+        self.root = Tkinter.Tk()
+        self.first_image = True
+        self.image = None
+        self.image_draw = None
 
-window.close()
+        # create canvas to display future image on
+        self.canvas = Tkinter.Canvas(width=512, height=512)
+        self.canvas.pack()
+
+        # call function to create black image on the canvas
+        self.create_new_black_image()
+
+        # use tag_bind to trigger pain_img to draw ellipse using the mouse
+        self.canvas.tag_bind(self.canvas._image_id, "<B1-Motion>", lambda e: self.paint_img(e))
+
+        # definition of buttons and their triggered functions
+        clear_button = Tkinter.Button(self.root, text="Clear image", command=self.create_new_black_image)
+        predict_button = Tkinter.Button(self.root, text="Predict", command=self.predict)
+        clear_button.pack()
+        predict_button.pack()
+
+        self.root.mainloop()
+
+    def create_new_black_image(self):
+        if not self.first_image:
+            pixels = self.image.load()
+            for i in range(self.image.size[0]):
+                for j in range(self.image.size[1]):
+                    pixels[i, j] = 0
+        else:
+            # create black image within the earlier defined canvas
+            self.image = Image.new('L', (512, 512), color="Black")
+            self.image_draw = ImageDraw.Draw(self.image)
+            self.canvas._image_tk = ImageTk.PhotoImage(self.image)
+            self.canvas._image_id = self.canvas.create_image(0, 0, image=self.canvas._image_tk, anchor='nw')
+            self.first_image = False
+
+    def paint_img(self, event):
+        x, y = event.x, event.y
+        self.image_draw.ellipse((x-10, y-10, x+10, y+10), fill='white')
+        self.canvas._image_tk = ImageTk.PhotoImage(self.image)
+        self.canvas.itemconfigure(self.canvas._image_id, image=self.canvas._image_tk)
+
+    def predict(self):
+        resized_image = self.image.resize((28, 28))
+        # resized_image.show()
+        numpy_array = np.array(resized_image)
+        plt.figure()
+        plt.imshow(numpy_array)
+        plt.colorbar()
+        plt.grid(False)
+        plt.show()
+
+
+Application()
+
--- model.py
+++ model.py
@@ -1,29 +1,43 @@
 from keras.datasets import mnist
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 
-(train_data, train_labels), (test_data, test_labels) = mnist.load_data()
-print(train_data.shape)
 
-plt.figure()
-plt.imshow(train_data[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Flatten(input_shape=(28, 28)),
-#     tf.keras.layers.Dense(128, activation='relu'),
-#     tf.keras.layers.Dense(10)
-# ])
-#
-# model.compile(optimizer='adam',
-#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#               metrics=['accuracy'])
-#
-# model.fit(train_data, train_labels, epochs=7)
-# test_loss, test_acc = model.evaluate(test_data, test_labels, verbose=2)
-#
-# print(test_acc, test_loss)
+class Model:
+    def __init__(self):
+        self.model = None
+        self.probability_model = None
+        self.number_of_epochs = 2
 
+    def define_and_train_model(self):
+        # loading the MNIST dataset for handwritten digits
+        (train_data, train_labels), (test_data, test_labels) = mnist.load_data()
+
+        # defining the model
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(10)
+        ])
+
+        # compiling the model
+        self.model.compile(optimizer='adam',
+                           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                           metrics=['accuracy'])
+
+        # training the model with MNIST dataset
+        self.model.fit(train_data, train_labels, epochs=self.number_of_epochs)
+        test_loss, test_acc = self.model.evaluate(test_data, test_labels, verbose= 0)
+
+        self.probability_model = tf.keras.Sequential([self.model,
+                                                      tf.keras.layers.Softmax()])
+        # print test accuracy
+        print(test_acc)
+
+    def make_prediction(self, predicting_image):
+        # reshape data so that it satisfies with the flattened input layer, and then make a prediction
+        reshaped_predicting_image = np.reshape(predicting_image, (1, 784))
+        prediction = np.argmax(self.probability_model.predict(reshaped_predicting_image))
+        predication_percentage = self.probability_model.predict(reshaped_predicting_image)[0][prediction] * 100
+        return prediction, predication_percentage
 
